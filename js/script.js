46a55bc21b533314589088aed9bcdfca3ea2ddd6
Marcelo Inácio <103380466+MarceloInacioJr@users.noreply.github.com>
2023-02-15 15:14:52 -0300

    0.0.2

--- js/script.js
+++ js/script.js
@@ -5,9 +5,18 @@ const grupoMeta = document.querySelector("#grupo-meta");
 const nomeMeta = document.querySelector("#nome-meta");
 const quantItem = document.querySelector("#qtd-item");
 const apelidoMetaConst = document.querySelector("#apelido");
+const btnAddTarefa = document.querySelector("#add-tarefa");
 const btnCadastrar = document.querySelector("#btn-cadastrar");
+let contador = 1;
 
 // funções 
+
+function apelidocontador() {
+    return contador++;
+}
+
+
+// modal
 function modalformulario() {
     var desaparecer = document.querySelector("#desaparecer");
     desaparecer.classList.toggle("desativar");
@@ -16,6 +25,45 @@ function modalformulario() {
     form.classList.toggle("desativar");
 }
 
+
+//função para adicionar tarefas
+btnAddTarefa.addEventListener("click", (e) => {
+    e.preventDefault();
+     
+    let contador = apelidocontador();
+    
+    if(contador >= 5){
+        btnAddTarefa.disabled = true;
+    }
+    else {
+    
+    console.log("contador", contador)
+    let formularioAddTarefas = document.querySelector(".form-add-tarefa");
+    
+    
+    let inputQItem = document.createElement("input");
+    inputQItem.setAttribute("type","number");
+    inputQItem.classList.add("qtd-item")
+    inputQItem.setAttribute("min", "1");
+    inputQItem.setAttribute("max", "10");
+    inputQItem.setAttribute("name", "qtd-item");
+    inputQItem.setAttribute("id", "qtd-item");
+    inputQItem.setAttribute("placeholder", "Quant. caixas")
+    formularioAddTarefas.appendChild(inputQItem);
+
+    let inputApelido =  document.createElement("input");
+    inputApelido.setAttribute("type", "text");
+    inputApelido.setAttribute("name", "apelido");
+    inputApelido.setAttribute("id", "apelido");
+    inputApelido.setAttribute("placeholder", "Apelido");
+
+    formularioAddTarefas.appendChild(inputApelido);
+   
+    }
+})
+
+
+// funcão para criar as metas
 btnCadastrar.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -24,7 +72,7 @@ btnCadastrar.addEventListener("click", (e) => {
     let nome = nomeMeta;
     let qItem = quantItem;
     let apelido = apelidoMetaConst;
-    let contador = 1;
+    
 
     // meta-conteudo
     let metas = document.querySelector(".metas");
@@ -55,28 +103,37 @@ btnCadastrar.addEventListener("click", (e) => {
     metasConteudo.appendChild(tituloNome);
 
 
-    // apelido
+    // apelido  
+
+    let caixaContent = document.createElement("div");
+    caixaContent.classList.add("caixa-content"); 
+    
+    let contador = apelidocontador();
+    for(let i = 1; i <= contador; i++){
+    console.log("feito")
+
     let apelidoMeta = document.createElement("div");
     apelidoMeta.classList.add("apelido-meta");
+    caixaContent.appendChild(apelidoMeta);
 
-    let apelidoP = document.createElement("p");
-    apelidoP.innerText = `${apelido.value} ${contador}`;
-    apelidoMeta.appendChild(apelidoP);
-    contador++;
+    let elementoApelido =  document.createElement("p");
+    elementoApelido.classList.add("elemento-apelido");
+    elementoApelido.innerText = apelido.value;    
+    apelidoMeta.appendChild(elementoApelido);
 
-    let apelidoCaixas = document.createElement("div");
-    apelidoCaixas.classList.add("caixas");
+    let caixas = document.createElement("div")
+    caixas.classList.add("caixas");
+    apelidoMeta.appendChild(caixas);
 
-    for (let i = 1; i <= qItem.value; i++) {
-        let caixa = document.createElement("div")
-        caixa.classList.add("caixa");
-        apelidoCaixas.appendChild(caixa);
+
+    for(let i = 1; i <= qItem.value ; i++){
+    let caixa = document.createElement("div");
+    caixa.classList.add("caixa");
+    caixas.appendChild(caixa);
+    metasConteudo.appendChild(caixaContent);
+
     }
-
-    apelidoMeta.appendChild(apelidoCaixas)
-    metasConteudo.appendChild(apelidoMeta);
-
-
-
+}
+     
 });
 
